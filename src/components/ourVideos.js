e4555fa908d61a1d238c6f69c93a089c9b48c775
AlejandroMar <lawebdealejo@gmail.com>
2022-01-17 10:52:07 +0100

    select video functionality

--- src/components/ourVideos.js
+++ src/components/ourVideos.js
@@ -1,14 +1,26 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Link } from 'gatsby';
+import { getYouTubeID } from '../utils/getYoutubeID';
 
-const videoGridData = [
+const videoGridUrls = [
     { link: 'https://www.youtube.com/embed/tJ55fohiR4g' },
     { link: 'https://www.youtube.com/embed/ta2nvtLAaOk' },
     { link: 'https://www.youtube.com/embed/BS0iBa_UAGs' },
     { link: 'https://www.youtube.com/embed/ZD3eOZZFIh8' },
 ];
 
+const videoGridData = videoGridUrls.map((url) => {
+    return { videoId: getYouTubeID(url.link) };
+});
+
 function OurVideos() {
+    const [videoId, setVideoId] = useState(videoGridData[1].videoId);
+
+    const selectVideo = (ID) => {
+        setVideoId(ID);
+    };
+
     return (
         <section className="videos my-5">
             <div className="container">
@@ -21,7 +33,7 @@ function OurVideos() {
                 <div className="ratio ratio-16x9 mb-4 m-auto w-75">
                     <iframe
                         allowFullScreen
-                        src="https://www.youtube.com/embed/ta2nvtLAaOk"
+                        src={`https://www.youtube.com/embed/${videoId}`}
                     />
                 </div>
 
@@ -29,14 +41,16 @@ function OurVideos() {
 
                 <div className="ml-auto mr-auto row w-100 videos__video-strip">
                     {videoGridData.map((videoData) => (
-                        <div className="col-6 mb-2 col-md-3">
-                            <div className="embed-responsive embed-responsive-16by9">
-                                <iframe
-                                    allowFullScreen
-                                    className="embed-responsive-item"
-                                    src={videoData.link}
-                                />
-                            </div>
+                        <div
+                            className="col-6 mb-2 col-md-3"
+                            key={videoData.videoId}
+                        >
+                            <img
+                                className="video_thumbnail w-100"
+                                src={`https://img.youtube.com/vi/${videoData.videoId}/mqdefault.jpg`}
+                                alt="video thumbnail"
+                                onClick={() => selectVideo(videoData.videoId)}
+                            />
                         </div>
                     ))}
                 </div>
