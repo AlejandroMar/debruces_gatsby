644c0d5a9c24688145d4c9556efe15a389bef17d
AlejandroMar <lawebdealejo@gmail.com>
2022-01-17 09:39:15 +0100

    refactor code to map

--- src/components/ourVideos.js
+++ src/components/ourVideos.js
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 
+const videoGridData = [
+    { link: 'https://www.youtube.com/embed/tJ55fohiR4g' },
+    { link: 'https://www.youtube.com/embed/ta2nvtLAaOk' },
+    { link: 'https://www.youtube.com/embed/BS0iBa_UAGs' },
+    { link: 'https://www.youtube.com/embed/ZD3eOZZFIh8' },
+];
+
 function OurVideos() {
     return (
         <section className="videos my-5">
@@ -19,42 +26,17 @@ function OurVideos() {
                 {/* video grid */}
 
                 <div className="ml-auto mr-auto row w-100 videos__video-strip">
-                    <div className="col-6 mb-2 col-md-3">
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe
-                                allowFullScreen
-                                className="embed-responsive-item"
-                                src="https://www.youtube.com/embed/tJ55fohiR4g"
-                            />
+                    {videoGridData.map((videoData) => (
+                        <div className="col-6 mb-2 col-md-3">
+                            <div className="embed-responsive embed-responsive-16by9">
+                                <iframe
+                                    allowFullScreen
+                                    className="embed-responsive-item"
+                                    src={videoData.link}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-6 mb-2 col-md-3">
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe
-                                allowFullScreen
-                                className="embed-responsive-item"
-                                src="https://www.youtube.com/embed/ta2nvtLAaOk"
-                            />
-                        </div>
-                    </div>
-                    <div className="col-6 mb-2 col-md-3">
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe
-                                allowFullScreen
-                                className="embed-responsive-item"
-                                src="https://www.youtube.com/embed/BS0iBa_UAGs"
-                            />
-                        </div>
-                    </div>
-                    <div className="col-6 mb-2 col-md-3">
-                        <div className="embed-responsive embed-responsive-16by9">
-                            <iframe
-                                allowFullScreen
-                                className="embed-responsive-item"
-                                src="https://www.youtube.com/embed/ZD3eOZZFIh8"
-                            />
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
             <div className="d-flex justify-content-center align-items-center my-5">
